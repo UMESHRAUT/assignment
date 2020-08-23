253c20851b8489abf95e9672e3ae07095d8561a9
UMESHRAUT <rautumesh34@gmail.com>
2020-08-23 08:29:13 +0530

    Updates

---
+++ precache-manifest.6c056fc68aa88618330c5ca2ee5cb7fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ac90482f2c287af08a92f7102c35366",
+    "url": "/assignment/index.html"
+  },
+  {
+    "revision": "ca769f26be2a42eaa4b4",
+    "url": "/assignment/static/css/main.2c0a8bca.chunk.css"
+  },
+  {
+    "revision": "056fe2522d76080a63e1",
+    "url": "/assignment/static/js/2.b821f640.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/assignment/static/js/2.b821f640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca769f26be2a42eaa4b4",
+    "url": "/assignment/static/js/main.ebfc4174.chunk.js"
+  },
+  {
+    "revision": "b1c92ba1d3c9949991c0",
+    "url": "/assignment/static/js/runtime-main.8d6cc747.js"
+  }
+]);
